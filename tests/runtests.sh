b314e8adf5f0715005b050402ff24da06ff9f7ac
CodePi <CodePi@users.noreply.github.com>
2017-02-11 02:41:25 -0500

    fixed test

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -6,11 +6,13 @@ echo --------------------------
 echo testThreadSafeQueue
 ./testThreadSafeQueue &
 pid=$!
+disown
 sleep 3
 kill $pid
 echo --------------------------
 echo testThreadSafeQueue2
 ./testThreadSafeQueue2 &
 pid=$!
+disown
 sleep 3
 kill $pid
